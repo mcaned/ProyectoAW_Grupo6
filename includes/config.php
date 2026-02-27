61305cb46a52d581d3593fc56a394dc11c344c8f
ggaya-spec <ggaya@ucm.es>
2026-02-27 14:11:52 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,7 @@ define('BD_PASS', 'awpass');
  * Parámetros de configuración utilizados para generar las URLs y las rutas a ficheros en la aplicación
  */
 define('RAIZ_APP', __DIR__);
-define('RUTA_APP', '/practica2');
+define('RUTA_APP', '/PROYECTO');
 define('RUTA_IMGS', RUTA_APP.'img/');
 define('RUTA_CSS', RUTA_APP.'css/');
 define('RUTA_JS', RUTA_APP.'js/');
@@ -22,4 +22,5 @@ define('RUTA_JS', RUTA_APP.'js/');
  */
 ini_set('default_charset', 'UTF-8');
 setLocale(LC_ALL, 'es_ES.UTF.8');
+
 date_default_timezone_set('Europe/Madrid');
