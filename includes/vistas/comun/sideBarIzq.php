602b4e248584ebaeef1a19e884fa43c665b72d88
ggaya-spec <ggaya@ucm.es>
2026-02-27 13:32:47 +0100

    Update sideBarIzq.php

--- includes/vistas/comun/sideBarIzq.php
+++ includes/vistas/comun/sideBarIzq.php
@@ -1,1 +1,8 @@
-
+<aside style="width: 200px; background-color: #d1d1d1; padding: 20px; min-height: 500px;">
+    <h3 style="margin-top: 0;">Navegación</h3>
+    <ul style="list-style-type: disc; color: red; padding-left: 20px;">
+        <li><a href="#" style="color: red; text-decoration: none;">Inicio</a></li>
+        <li><a href="#" style="color: red; text-decoration: none;">Ver contenido</a></li>
+        <li><a href="#" style="color: red; text-decoration: none;">Administrar</a></li>
+    </ul>
+</aside>
