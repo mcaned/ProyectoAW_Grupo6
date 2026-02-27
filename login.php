a3dc3263319f6b2714593565af669f16f1234df4
ggaya-spec <ggaya@ucm.es>
2026-02-27 15:03:43 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,9 +1,12 @@
 <?php
 require_once __DIR__ . '/includes/config.php';
+require_once __DIR__ . '/includes/Aplicacion.php';
 require_once __DIR__ . '/includes/FormularioLogin.php';
 
-$tituloPagina = 'Login - Bistro FDI';
+$app = Aplicacion::getInstance(); 
+$app->init();
 
+$tituloPagina = 'Login - Bistro FDI';
 $form = new FormularioLogin();
 $htmlFormulario = $form->gestiona();
 
@@ -11,16 +14,11 @@ include __DIR__ . '/includes/vistas/comun/cabecera.php';
 ?>
 
 <div style="display: flex; min-height: 80vh;">
-    <?php include __DIR__ . '/includes/vistas/comun/sidebarIzq.php'; ?>
-
-    <main style="flex-grow: 1; padding: 40px; background: white;">
+    <?php include __DIR__ . '/includes/vistas/comun/sidebarIzq.php'; ?> <main style="flex-grow: 1; padding: 40px; background: white;">
         <h1>Acceso al sistema</h1>
         <?= $htmlFormulario ?>
     </main>
 
-    <?php include __DIR__ . '/includes/vistas/comun/sidebarDer.php'; ?>
-</div>
+    <?php include __DIR__ . '/includes/vistas/comun/sidebarDer.php'; ?> </div>
 
-<?php 
-include __DIR__ . '/includes/vistas/comun/pie.php'; 
-?>
+<?php include __DIR__ . '/includes/vistas/comun/pie.php'; ?>
