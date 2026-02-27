79ea21930342898b71a038a58d65dde5f448cd13
ggaya-spec <ggaya@ucm.es>
2026-02-27 14:15:05 +0100

    Add files via upload

--- includes/vistas/plantillas/plantilla.php
+++ includes/vistas/plantillas/plantilla.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<!--  <link rel="stylesheet" type="text/css" href="estilo.css" /> -->
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title><?= $tituloPagina ?></title>
+</head>
 
+<body>
+
+<div id="contenedor">
+  <main>
+     <article>
+	    <?= $contenidoPrincipal ?>
+	 </article>
+  </main>
+  <footer>
+	<p>[c]::<?= $contenidoPie ?>
+  </footer>
+</div>
+
+</body>
+</html>
