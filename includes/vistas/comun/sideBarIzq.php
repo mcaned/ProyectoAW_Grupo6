7442e9560cc4e2496c7c00b66a19628053d352b6
ggaya-spec <ggaya@ucm.es>
2026-02-28 14:44:06 +0100

    Add files via upload

--- includes/vistas/comun/sideBarIzq.php
+++ includes/vistas/comun/sideBarIzq.php
@@ -1,7 +1,7 @@
 <aside style="width: 200px; background-color: #d1d1d1; padding: 20px; min-height: 500px;">
     <h3 style="margin-top: 0;">Navegación</h3>
     <ul style="list-style-type: disc; color: red; padding-left: 20px;">
-        <li><a href="#" style="color: red; text-decoration: none;">Inicio</a></li>
+        <li><a href="../login.php" style="color: red; text-decoration: none;">Inicio</a></li>
         <li><a href="#" style="color: red; text-decoration: none;">Ver contenido</a></li>
         <li><a href="#" style="color: red; text-decoration: none;">Administrar</a></li>
     </ul>
