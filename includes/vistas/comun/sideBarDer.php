20966f553a3bc586b8f6e330887d6685112c0cbe
Marta <mcaned01@ucm.es>
2026-03-02 12:01:21 +0100

    Update sideBarDer.php

--- includes/vistas/comun/sideBarDer.php
+++ includes/vistas/comun/sideBarDer.php
@@ -1,3 +1,3 @@
-<aside style="width: 200px; background-color: #d1d1d1; padding: 10px; color: #666; font-size: 0.9rem;">
+<aside style="width: 200; background-color: dark white; padding: 10 ;color: light grey; font-size: 13;">
     <p>Texto del sidebar derecho.</p>
 </aside>
