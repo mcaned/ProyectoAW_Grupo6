ebe73ce61d570593315bee5127a7c5c653162530
Marta <mcaned01@ucm.es>
2026-03-02 19:41:30 +0100

    Update sideBarIzq.php

--- includes/vistas/comun/sideBarIzq.php
+++ includes/vistas/comun/sideBarIzq.php
@@ -1,17 +1,17 @@
-<div style="padding: 20;">
-    <ul style="list-style: none; padding: 0;">
-        <li style="margin-bottom: 15;">
-            <a href="<?= RUTA_APP ?>/includes/carta.php" style=" text-decoration: none; color: dark grey; font-weight: bold;">🍴 Carta</a>
+<div style="padding: 20px; background-color: #cccccc4a; border-radius: 8px;">
+    <ul style="list-style: none; padding: 0; margin: 0;">
+        <li style="margin-bottom: 15px;">
+            <a href="<?= RUTA_APP ?>/includes/carta.php" style="text-decoration: none; color: #555; font-weight: bold;">🍴 Carta</a>
         </li> 
         
-        <li style="margin-bottom: 15;">
-            <a href="<?= RUTA_APP ?>/includes/pedido.php" style="text-decoration: none; color: dark grey; font-weight: bold;">
+        <li style="margin-bottom: 15px;">
+            <a href="<?= RUTA_APP ?>/includes/pedido.php" style="text-decoration: none; color: #555; font-weight: bold;">
                 📋 <?= (isset($_SESSION['rol']) && $_SESSION['rol'] === 'gerente') ? 'Gestionar Pedidos' : 'Ver mis pedidos' ?>
             </a>
         </li>
 
-        <li style="margin-bottom: 15;">
-            <a href="<?= RUTA_APP ?>/includes/carrito.php" style="text-decoration: none; color: dark grey; font-weight: bold;">🛒 Carrito</a>
+        <li style="margin-bottom: 15px;">
+            <a href="<?= RUTA_APP ?>/includes/carrito.php" style="text-decoration: none; color: #555; font-weight: bold;">🛒 Carrito</a>
         </li>
     </ul>
 </div>
