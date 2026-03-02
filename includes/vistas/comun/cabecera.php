a3e6aa9ddfeb8081c2a0e2647900b1ff4701ad74
Marta <mcaned01@ucm.es>
2026-03-02 12:00:25 +0100

    Update cabecera.php

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -1,16 +1,56 @@
-<header style="background-color: #333; color: white; padding: 40px; position: relative; text-align: center;">
-    <div style="position: absolute; left: 40px; top: 50%; transform: translateY(-50%);">
-        <img src="<?= RUTA_APP ?>/img/logo.png" style="width: 280px;">
+<style>
+    .header-bistro {
+        background-color: grey;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .header-section {
+        flex: 1; 
+    }
+
+    
+    .header-logo img { /*Tenemos a la izq el logo*/
+        width: 230px;
+        height: auto;
+        display: block;
+    }
+
+    
+    .header-titles { /*Centro texto*/
+        text-align: center;
+    }
+    .header-titles h1 { margin: 0; font-size: 40; }
+    .header-titles h2 { margin: 0; font-size: 17; color: white; font-style: italic; }
+
+    
+    .header-links { /* Derecha Login */
+        text-align: right;
+        font-size: 17;
+        padding-right: 20; //crea espacio a la derecha
+    }
+    .header-links a { color: white;  font-weight: bold; }
+    .header-links a:hover { text-decoration: underline; } /*subrayamos salir*/
+</style>
+
+<header class="header-bistro">
+    <div class="header-section header-logo">
+        <img src="<?= RUTA_APP ?>/img/logo.png" alt="Logo">
     </div>
-    <h1>BISTRO FDI</h1>
-    <h2 style="font-style: italic; color: #ccc;">¿Te gusta el pisto?</h2>
-    <div style="position: absolute; bottom: 10px; right: 20px;">
-        <?php if (isset($_SESSION['login'])): ?>
-            Bienvenido, <?= htmlspecialchars($_SESSION['nombre']) ?> | 
-            <a href="<?= RUTA_APP ?>/logout.php" style="color:white;">Salir</a>
+
+    <div class="header-section header-titles">
+        <h1>BISTRO FDI</h1>
+        <h2>¿Te gusta el pisto?</h2>
+    </div>
+
+    <div class="header-section header-links">
+        <?php if (isset($_SESSION['login'])): ?> 
+            Bienvenido, <?= htmlspecialchars($_SESSION['nombre'])?> | 
+            <a href="<?= RUTA_APP ?>/logout.php">Salir</a>
         <?php else: ?>
-            <a href="<?= RUTA_APP ?>/login.php" style="color:white;">Login</a> | 
-            <a href="<?= RUTA_APP ?>/registro.php" style="color:white;">Registro</a>
+            <a href="<?= RUTA_APP ?>/login.php">Login</a> | 
+            <a href="<?= RUTA_APP ?>/registro.php">Registro</a>
         <?php endif; ?>
     </div>
 </header>
