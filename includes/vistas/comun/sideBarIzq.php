ecf7c444dac898d4dbb0053f8a450aa948063672
Angelg115 <anggal05@ucm.es>
2026-03-07 18:31:26 +0100

    Update sideBarIzq.php
    Añadido el boton para el gerente

--- includes/vistas/comun/sideBarIzq.php
+++ includes/vistas/comun/sideBarIzq.php
@@ -13,6 +13,12 @@
         <li style="margin-bottom: 15px;">
             <a href="<?= RUTA_APP ?>/includes/carrito.php" style="text-decoration: none; color: #555; font-weight: bold;">🛒 Carrito</a>
         </li>
+         <!-- BOTÓN EXTRA SOLO PARA EL GERENTE (Estilo igualado) -->
+        <?php if (isset($_SESSION['login']) && $_SESSION['rol'] === 'gerente'): ?>
+        <li style="margin-bottom: 15px; border-top: 1px solid #ccc; padding-top: 15px;">
+            <a href="<?= RUTA_APP ?>/admin.php" style="text-decoration: none; color: #555; font-weight: bold;">⚙️ Panel de Administración</a>
+        </li>
+        <?php endif; ?>
     </ul>
 </div>
 <!--text-decoration lo ponemos a none para que no se nos visualice la linea -->
