0384741da67bab40cb1398191296b5fcb2ef9b0e
ggaya-spec <ggaya@ucm.es>
2026-03-09 18:46:21 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/includes/config.php';
-require_once __DIR__ . '/includes/Aplicacion.php';
-require_once __DIR__ . '/includes/FormularioLogin.php';
+require_once __DIR__ . '/includes/aplicacion.php';
+require_once __DIR__ . '/includes/formularioLogin.php';
 
 $app = Aplicacion::getInstance(); 
 $app->init();
@@ -20,4 +20,5 @@ include __DIR__ . '/includes/vistas/comun/cabecera.php';
     </main>
     <?php include __DIR__ . '/includes/vistas/comun/sidebarDer.php'; ?> 
 </div>
+
 <?php include __DIR__ . '/includes/vistas/comun/pie.php'; ?>
