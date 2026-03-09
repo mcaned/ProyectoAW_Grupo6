f4fb6587a5d7e50062618e5ec0fbd6c04b0931e1
Marta <mcaned01@ucm.es>
2026-03-09 11:56:18 +0100

    Update pie.php

--- includes/vistas/comun/pie.php
+++ includes/vistas/comun/pie.php
@@ -1,7 +1,7 @@
 <footer class="pie-principal">
     <p >
         &copy; <?php echo date("Y"); ?> - <strong> Mi gran página web</strong> 
-        | Hecho por: Gonzalo Gayá, Marta Cañedo, Felipe López y Ángel Gallego.
+        | Hecho por: Gonzalo Gayá, Marta Cañedo, Ángel Gallego y Felipe López .
     </p>
 </footer>
 </body>
