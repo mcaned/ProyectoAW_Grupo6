105fac564b988842e864beaf748b3453cf60ac68
ggaya-spec <ggaya@ucm.es>
2026-03-12 12:07:53 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -12,10 +12,10 @@ define('BD_PASS', 'Grupo06');
  * Parámetros de configuración utilizados para generar las URLs y las rutas a ficheros en la aplicación
  */
 define('RAIZ_APP', __DIR__);
-define('RUTA_APP', '/ProyectoAW_Grupo6-main');
+define('RUTA_APP', '');
 define('RUTA_IMGS', RUTA_APP.'img/');
-define('RUTA_CSS', RUTA_APP.'css/');
-define('RUTA_JS', RUTA_APP.'js/');
+define('RUTA_CSS', RUTA_APP.'CSS/');
+define('RUTA_JS', RUTA_APP.'JS/');
 
 /**
  * Configuración del soporte de UTF-8, localización (idioma y país) y zona horaria
@@ -24,3 +24,4 @@ ini_set('default_charset', 'UTF-8');
 setLocale(LC_ALL, 'es_ES.UTF.8');
 
 date_default_timezone_set('Europe/Madrid');
+
