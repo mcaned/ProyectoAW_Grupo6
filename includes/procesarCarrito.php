ff44e77a7fb6f3f74636326776d9727ec9456f6a
Marta <mcaned01@ucm.es>
2026-03-22 19:44:19 +0100

    Add files via upload

--- includes/procesarCarrito.php
+++ includes/procesarCarrito.php
@@ -12,21 +12,41 @@ if (!isset($_SESSION['login'])) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id_producto = $_POST['id_producto'] ?? null;
+    $action = $_POST['action'] ?? 'add'; 
     $cantidad = (int)($_POST['cantidad'] ?? 1);
 
-    if ($id_producto && $cantidad > 0) {
-        
+    if ($id_producto) {
         if (!isset($_SESSION['carrito'])) {
             $_SESSION['carrito'] = [];
         }
 
-        if (isset($_SESSION['carrito'][$id_producto])) {
-            $_SESSION['carrito'][$id_producto] += $cantidad;
-        } else {
-            $_SESSION['carrito'][$id_producto] = $cantidad;
+        switch ($action) {
+            case 'add':
+                if (isset($_SESSION['carrito'][$id_producto])) {
+                    $_SESSION['carrito'][$id_producto] += $cantidad;
+                } else {
+                    $_SESSION['carrito'][$id_producto] = $cantidad;
+                }
+                header("Location: carta.php?añadido=1");
+                exit();
+
+            case 'update':
+                //(asegurando que no sea menor a 1)
+                if ($cantidad > 0) {
+                    $_SESSION['carrito'][$id_producto] = $cantidad;
+                } else {
+                    unset($_SESSION['carrito'][$id_producto]);
+                }
+                header("Location: carrito.php");
+                exit();
+
+            case 'remove':
+                unset($_SESSION['carrito'][$id_producto]);
+                header("Location: carrito.php");
+                exit();
         }
     }
 }
 
-header("Location: carta.php?añadido=1");
+header("Location: carta.php");
 exit();
